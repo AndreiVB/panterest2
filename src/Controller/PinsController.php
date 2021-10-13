1b4890dbb1b4b8313d5d3df0cb2fb7ef9cc0205d
AndreiWWW <andreiv.banut@gmail.com>
2021-10-13 18:26:59 +0300

    add create/edit, add navigation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,14 +4,24 @@ namespace App\Controller;
 
 use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
+
+    //if lot of $em use(optional), istead of injecting in every function
+    /* private $em;
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em; 
+    } */
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods="GET")
      */
     public function index(PinRepository $pinRepository): Response
     {
@@ -20,12 +30,66 @@ class PinsController extends AbstractController
         return $this->render('pins/index.html.twig', compact('pins'));
     }
 
+    /**
+     * @Route("/pins/create", name="app_pins_create", methods="GET|POST")
+     */
+    // methods={"GET", "POST"} same as line 27
+    public function create(Request $request, EntityManagerInterface $em): Response {
+        $pin = new Pin; 
+        $form = $this->createFormBuilder($pin)
+         ->add('title', TextType::class)
+         ->add('description', TextareaType::class)
+         // the button recommended in view, not in controller
+         ->getForm()         
+        ;
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+        // is recommended to use dependance injection in function arguments eg:line 31
+        // $em = $this->getDoctrine()->getManager(); 
+        $em->persist($pin);
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+
+        }
+        return $this->render('pins/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
     
     /**
-     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show", methods="GET")
      */
     public function show(Pin $pin): Response
     {
        return $this->render('pins/show.html.twig', compact('pin'));
     }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/edit", name="app_pins_edit", methods={"GET", "POST"})
+     */
+    public function edit(Pin $pin, Request $request, EntityManagerInterface $em): Response
+    {
+
+        $form = $this->createFormBuilder($pin)
+         ->add('title', TextType::class)
+         ->add('description', TextareaType::class)
+         ->getForm()  ;       
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) { 
+        $em->flush();
+
+        return $this->redirectToRoute('app_home');
+            
+    }
+      return $this->render('pins/edit.html.twig', [
+          'pin' => $pin,
+          'form' => $form->createView()
+      ]);
+}
 }
