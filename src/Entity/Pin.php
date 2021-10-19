896d342dd642f1f07ccb67df611333b34fdf658a
AndreiWWW <andreiv.banut@gmail.com>
2021-10-19 16:50:53 +0300

    Add validation in Pin entity annotations

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,6 +6,7 @@ use App\Entity\Traits\Timestampable;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PinRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
@@ -25,11 +26,15 @@ class Pin
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Title can't be blank")
+     * @Assert\Length(min=3)
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
+     * @Assert\Length(min=10)
      */
     private $description;
 
