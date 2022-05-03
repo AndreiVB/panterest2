1667f0173258deac8d784d6a3a730fd0551f145f
AndreiWWW <andreiv.banut@gmail.com>
2022-05-04 02:23:35 +0300

    added users/pins psysh, dinamyc show pin owner in twig
    added some users and pins to test(with psysh, fixtures to take in account in the future, new function to User, getFullName to be displayed in twig

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -91,6 +91,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getFullName(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+
+
     public function getEmail(): ?string
     {
         return $this->email;
