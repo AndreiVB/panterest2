2958bb1bccb3d70974a485f16048298f3cb1a26e
AndreiWWW <andreiv.banut@gmail.com>
2022-05-17 00:37:58 +0300

    styled reset views, made migration with pwd_reset_requests

--- src/Entity/ResetPasswordRequest.php
+++ src/Entity/ResetPasswordRequest.php
@@ -9,6 +9,7 @@ use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestTrait;
 
 /**
  * @ORM\Entity(repositoryClass=ResetPasswordRequestRepository::class)
+ * @ORM\Table(name="reset_password_requests")
  */
 class ResetPasswordRequest implements ResetPasswordRequestInterface
 {
